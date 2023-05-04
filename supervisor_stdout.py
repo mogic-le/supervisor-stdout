e3bd5efe0ddcbf341fb9ce7a8b740492cac02633
Maximilian Weber <weber@mogic.com>
2023-05-04 15:04:21 +0200

    Add support for python3 and supervisor 4

--- supervisor_stdout.py
+++ supervisor_stdout.py
@@ -15,15 +15,14 @@ def main():
         line = sys.stdin.readline()  # read header line from stdin
         headers = dict([ x.split(':') for x in line.split() ])
         data = sys.stdin.read(int(headers['len'])) # read the event payload
-        write_stdout('RESULT %s\n%s'%(len(data.encode("utf-8")), data)) # transition from READY to ACKNOWLEDGED
+        write_stdout('RESULT %s\n%s'%(len(data.encode('utf-8')), data)) # transition from READY to ACKNOWLEDGED
 
 def event_handler(event, response):
-    response = response.decode()
-    line, data = response.split('\n', 1)
-    headers = dict([ x.split(':') for x in line.split() ])
-    lines = data.split('\n')
-    prefix = '%s %s | '%(headers['processname'], headers['channel'])
-    print('\n'.join([ prefix + l for l in lines ]))
+    line, data = response.split(b'\n', 1)
+    headers = dict([ x.split(b':') for x in line.split() ])
+    lines = data.split(b'\n')
+    prefix = b'%s %s | '%(headers[b'processname'], headers[b'channel'])
+    print(b'\n'.join([ prefix + l for l in lines if l]).decode('utf-8'))
 
 if __name__ == '__main__':
     main()
